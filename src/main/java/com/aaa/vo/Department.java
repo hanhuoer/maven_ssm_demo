3b96f1c5e18407db060e05d0a31f57db2a83942b
shaokang <1206751678@qq.com>
2018-12-31 20:10:03 +0800

    Initial commit

---
+++ src/main/java/com/aaa/vo/Department.java
@@ -0,0 +1,19 @@
+package com.aaa.vo;
+
+import lombok.Data;
+import lombok.ToString;
+
+
+@Data
+@ToString
+public class Department {
+
+	private int id;
+
+	private String name;
+
+	private String desc;
+
+	private int state;
+	
+}
